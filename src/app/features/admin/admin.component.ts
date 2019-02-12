ed475319f9a2534ba123abea64a3412ef59c2ba8
Roberto Zanotto <robyzan8@gmail.com>
2019-02-12 14:47:33 +0100

    moved from template to templateUrl

--- src/app/features/admin/admin.component.ts
+++ src/app/features/admin/admin.component.ts
@@ -2,70 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-admin',
-  template: `
-    <!--player-->
-    <div class="center">
-      <img
-        class="player-image"
-        src="https://picsum.photos/600/400?image=10"
-      >
-
-      <div>
-        <button class="button">PREV</button>
-        <span class="counter"> 1 / 3</span>
-        <button class="button">NEXT</button>
-      </div>
-    </div>
-
-    <!--gallery-->
-    <div class="gallery">
-
-      <div class="center">
-        <form>
-          <input type="text" placeholder="Add new image URL">
-          <button class="button" type="button">Generate Image</button>
-        </form>
-      </div>
-
-      <div class="image-container">
-        <div class="grid">
-          <div class="cell">
-            <img class="responsive-image" src="https://picsum.photos/600/400?image=10">
-            <span class="icon cursor">✖</span>
-          </div>
-
-          <div class="cell">
-            <img class="responsive-image" src="https://picsum.photos/600/400?image=10">
-            <span class="icon cursor">✖</span>
-          </div>
-
-
-          <div class="cell">
-            <img class="responsive-image" src="https://picsum.photos/600/400?image=10">
-            <span class="icon cursor">✖</span>
-          </div>
-
-
-          <div class="cell">
-            <img class="responsive-image" src="https://picsum.photos/600/400?image=10">
-            <span class="icon cursor">✖</span>
-          </div>
-
-          <div class="cell">
-            <img class="responsive-image" src="https://picsum.photos/600/400?image=10">
-            <span class="icon cursor">✖</span>
-          </div>
-
-
-          <div class="cell">
-            <img class="responsive-image" src="https://picsum.photos/600/400?image=10">
-            <span class="icon cursor">✖</span>
-          </div>
-
-        </div>
-      </div>
-    </div>
-  `,
+  templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent implements OnInit {
--- src/app/features/viewer/viewer.component.ts
+++ src/app/features/viewer/viewer.component.ts
@@ -2,11 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-viewer',
-  template: `
-    <div class="bg">
-      <img src="https://picsum.photos/600/400?image=10">
-    </div>
-  `,
+  templateUrl: './viewer.component.html',
   styleUrls: ['./viewer.component.css']
 })
 export class ViewerComponent implements OnInit {
