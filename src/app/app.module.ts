3f66d37212bed18a4b4ba83d9491ce963ebf21ca
Roberto Zanotto <robyzan8@gmail.com>
2019-02-12 16:01:00 +0100

    addition and deletion of images implemented in /admin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminComponent } from './features/admin/admin.component';
 import { ViewerComponent } from './features/viewer/viewer.component';
-import { AngularFireModule } from '@angular/fire'
-import { AngularFireDatabaseModule } from '@angular/fire/database'
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
 import { environment } from 'src/environments/environment';
+import { FormsModule } from '@angular/forms'
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { environment } from 'src/environments/environment';
     BrowserModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
-    AngularFireDatabaseModule
+    AngularFireDatabaseModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/features/admin/admin.component.ts
+++ src/app/features/admin/admin.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { defineBase } from '@angular/core/src/render3';
 
 @Component({
   selector: 'app-admin',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent implements OnInit {
+  items: string[] = [];
 
-  constructor() { }
+  constructor(private db: AngularFireDatabase) { }
 
   ngOnInit() {
+    this.db.object('presentation').valueChanges().subscribe((val: any) => {
+      //console.log(val);
+      this.items = val.images;
+    });
   }
 
+  addImage(url: string) {
+    this.items = [...this.items, url]
+    this.db.object('presentation/images').set(this.items)
+  }
+
+  generateRandomImage() {
+    let r = Math.floor(Math.random()*200)
+    let url = 'https://picsum.photos/600/400?image=' + r
+    this.addImage(url)
+  }
+
+  delete(url: string) {
+    this.items = this.items.filter(item => item !== url)
+    this.db.object('presentation/images').set(this.items)
+  }
 }
